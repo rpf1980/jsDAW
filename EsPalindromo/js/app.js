eca64eb4b42b77552ac156c4ba9ef4447c0a52f6
rpf1890 <united_ssh@outlook.com>
2019-07-02 23:48:17 +0200

    Ejercicio Palíndromos terminado

---
+++ EsPalindromo/js/app.js
@@ -0,0 +1,62 @@
+const idInput = document.getElementById('idInput');
+const idDiv = document.getElementById('idDiv');
+
+// Métodos aparte
+function invierteString(cadena)
+{
+    let copia = "";
+
+    for(i = cadena.length-1; i >= 0; i--)
+    {
+        copia = copia + cadena.charAt(i);
+    }
+    return copia;
+}
+
+function quitaAcentos(cadena)
+{
+    let copia = "";
+    const con = "áàäéèëíìïóòöúùuñÁÀÄÉÈËÍÌÏÓÒÖÚÙÜÑçÇ";
+    const sin = "aaaeeeiiiooouuunAAAEEEIIIOOOUUUNcC";
+    let contador = 0;
+
+    for(i = 0; i < cadena.length; i++)
+    {
+        for(j = 0; j < con.length; j++)
+        {
+            if(cadena.charAt(i) === con.charAt(j))
+            {
+                copia = cadena.replace(cadena.charAt(i), sin.charAt(j));
+            }
+        }
+    }
+    return copia;
+}
+
+// Evento del input 
+idInput.addEventListener('change', function(event)
+{
+    const str = event.target.value;
+
+    let texto = quitaAcentos(str);
+
+    let quitaEspacios = texto.replace(/ /g, '');
+
+    let todoMinusculas = quitaEspacios.toLowerCase();
+
+    let stringInvertido = invierteString(todoMinusculas);
+
+    //Creamos un párrafo
+    let p = document.createElement("p");
+
+    if(todoMinusculas === stringInvertido)
+    {
+        p.textContent = "El cadena de texto es un palíndromo";
+        idDiv.appendChild(p);
+    }
+    else
+    {
+        p.textContent = "La cadena de texto NO es un palíndromo";
+        idDiv.appendChild(p);
+    }
+})
