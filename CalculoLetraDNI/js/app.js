c7d5863cc04184f41a2d3780536e1574825d666b
rpf1890 <united_ssh@outlook.com>
2019-07-02 11:50:00 +0200

    Hechos hasta Palíndromo, este mismo incluido

--- CalculoLetraDNI/js/app.js
+++ CalculoLetraDNI/js/app.js
@@ -1,12 +1,25 @@
 const idDiv = document.getElementById("idDiv");
+const idBtnLimpiar = document.getElementById("idBtnLimpiar");
 
-const letras = "T,R,W,A,G,M,Y,F,P,D,X,B,N,J,Z,S,Q,V,H,L,C,K,E";
+const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+const array = Array.from(letras);
 
 function letraDNI(n)
 {
     // Sacamos el resto que nos da número DNI / 23
     const resto = n % 23;
 
-    console.log(resto);
+    // Con la posición = resto sacamos la letra correpondiente del DNI
+    const resultado = array[resto];
 
+    idDiv.innerHTML += "Nuestra letra es la " + resultado;
 }
+
+idBtnLimpiar.addEventListener("click", function()
+{
+    idDiv.innerHTML = "";
+})
+
+
+
+
