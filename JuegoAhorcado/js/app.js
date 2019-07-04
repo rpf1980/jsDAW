058cc701147518baf0ac83f8eae393e86b526177
rpf1980 <united_ssh@outlook.com>
2019-07-04 22:37:38 +0200

    Queda por terminar... cambios sin importancia

--- JuegoAhorcado/js/app.js
+++ JuegoAhorcado/js/app.js
@@ -7,41 +7,30 @@ const textoPalabra = parrafo.textContent;
 // Guardamos texto completo de la palabra ( solución )
 const textoSolucion = "E l e c t r o d o m e s t i c o";
 
-idLetra.addEventListener('keypress', function(event)
-{
+idLetra.addEventListener('keypress', function (event) {
     const inputChar = event.target.value;
 
-    if(event.key === 'Enter')
-    {
-        //Buscamos la letra que se escribe en el input sobre la cadena de texto (la palabra del juego)
-        
-        if(textoSolucion.includes("inputChar"))
-        {
-            const posLetra = textoSolucion.indexOf("inputChar");
+    if (event.key === 'Enter') {
 
-            textoPalabra.replace(/_/gi, 'posLetra');
+        for (i = 0; i < textoPalabra.length; i++) {
 
+            if (textoSolucion.includes("inputChar")) {
+
+                const patron = /_/g;
+
+                const nuevaCadena = textoPalabra.replace(patron, inputChar);
+            }
         }
     }
 })
 
 // Funciones aparte
-function cuantasVecesCaracter(cadena, caracter)
-{
-    const veces = cadena.split(caracter).length-1;
+function cuantasVecesCaracter(cadena, caracter) {
+    const veces = cadena.split(caracter).length - 1;
     return veces;
 }
 
-function sustituyeCaracteres(cadena, viejo, nuevo)
-{
-    const copia = cadena;
-
-    for(i = 0; i < copia.length; i++)
-    {
-        if(copia.charAt(i) === viejo)
-        {
-            
-        }
-    }
-    return copia;
+function sustituyeCaracteres() {
+    
+    
 }
