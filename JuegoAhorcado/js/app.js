7e53e08cdf67ac54bdb073341e99cff9c3e52603
rpf1890 <united_ssh@outlook.com>
2019-07-05 15:58:46 +0200

    Pasamos cadenas a arrays para poder manejar mejor los métodos asociados a los mismos (arrays)

--- JuegoAhorcado/js/app.js
+++ JuegoAhorcado/js/app.js
@@ -8,51 +8,32 @@ const textoPalabra = parrafo.textContent;
 // Guardamos texto completo de la palabra ( solución )
 const textoSolucion = "E l e c t r o d o m e s t i c o";
 
-idLetra.addEventListener('keypress', function (event) {
-    const inputChar = event.target.value;
-    let cadenaNueva = "";
-    
+// Pasamos a minúsculas los dos texto
+const textMin = textoPalabra.toLowerCase();
+const textMin2 = textoSolucion.toLowerCase();
 
+// Hacemos split por los espacios a los dos string
+const arrayTextoJuego = textMin.split(" ");
+const arrayTextoSolucion = textMin2.split(" ");
+
+console.log(arrayTextoJuego);
+console.log(arrayTextoSolucion);
+
+idLetra.addEventListener('keypress', function (event) {
+    // Valor de la letra que escribimos en el input
+    const inputChar = event.target.value;
+
+    // Entramos si pulsamos enter
     if (event.key === 'Enter') {
 
-        if (textoSolucion.includes(inputChar)) {
+        // Recorremos el arrayTextoSolucion para comparar la letra del input
+        if(arrayTextoSolucion.includes(inputChar)){
 
-            cadenaNueva = sustituyeCaracteres(textoPalabra, /_/gi, inputChar);
-            parrafo.textContent = cadenaNueva; // Reseteamos el párrafo con los cambios ( contiene la palabra oculta del juego )
+            
+
         }
+
     }
 })
 
 // Funciones aparte
-function cuantasVecesCaracter(cadena, caracter) {
-    const veces = cadena.split(caracter).length - 1;
-    return veces;
-}
-
-function sustituyeCaracteres(cadena, patron, caracter) {
-    
-    let nuevaCadena = cadena.replace(patron, caracter);
-
-    return nuevaCadena;
-    
-}
-
-function guardaPosicionesCaracter(cadena, caracter)
-{
-    let arrayPosiciones = [];
-    let cadenaNueva = "";
-
-    for(i = 0; i < cadena.length; i++)
-    {
-        if(cadena.charAt(i) == caracter)
-        {
-            let pos = cadena.indexOf(charAt(i));  // Guardamos posición del caracter que buscamos
-            arrayPosiciones.add(i); // Guardamos esa posición en arrayPosiciones
-            cadenaNueva = sustituyeCaracteres(cadena, /caracter/g, "x");
-        }
-
-        guardaPosicionesCaracter(cadena, caracter);
-    }
-
-    return arrayPosiciones;
-}
