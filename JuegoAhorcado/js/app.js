eab7b1805552c0274e121d58484055d8a0dcc425
rpf1980 <united_ssh@outlook.com>
2019-07-07 19:12:07 +0200

    En este punto estamos tomando todas las posiciones del caracter encontrado, usando la función aparte arrayPosicionesCaracter

--- JuegoAhorcado/js/app.js
+++ JuegoAhorcado/js/app.js
@@ -12,12 +12,10 @@ const textoSolucion = "E l e c t r o d o m e s t i c o";
 const textMin = textoPalabra.toLowerCase();
 const textMin2 = textoSolucion.toLowerCase();
 
-// Hacemos split por los espacios a los dos string
+// Hacemos split por los espacios a los dos string y los pasamos a array
 const arrayTextoJuego = textMin.split(" ");
 const arrayTextoSolucion = textMin2.split(" ");
 
-console.log(arrayTextoJuego);
-console.log(arrayTextoSolucion);
 
 idLetra.addEventListener('keypress', function (event) {
     // Valor de la letra que escribimos en el input
@@ -40,15 +38,23 @@ idLetra.addEventListener('keypress', function (event) {
 
 // Funciones aparte
 function arrayPosicionesCaracter(arrayOriginal, char)
-{
-    let contador = 0;
+{   // Con esta función pretendemos guardar todas las posiciones que
+    // ocupan en el array el caracter que pasamos por parámetro
+
+    let pos = -1;
+    let arrayDePosiciones = new Array();
 
     for(i = 0; i < arrayOriginal.length; i++)
     {
-        if(arrayOriginal[i] === char)
+        if(arrayOriginal[i] === char) // Si una de las posiciones del array es el caracter (será la primera que encuentre)
         {
-            contador++;
+            pos = arrayOriginal.indexOf(arrayOriginal[i]);  // Tomamos la posición del caracter ( caracter del array que coincide con el caracter que vamos buscando )
+            arrayDePosiciones.push(pos); // Guardamos la posición en el array
+            arrayOriginal.splice(pos,1,'x'); // En este punto debemos sustituir el caracter por otro ( ejemplo una x ) ya que indexOf solo devuelve la posición del primer caracter encontrado y queremos todas las posiciones por si hay más caracteres iguales
+
         }
+
+        arrayPosicionesCaracter(arrayOriginal, char); // Llamada recursiva de la función para que siga hasta que acaben las interaciones del for
     }
     
 }
