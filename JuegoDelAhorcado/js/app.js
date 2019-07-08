923483a6c949f5fa5b86c547dbddd0477a582725
rpf1980 <united_ssh@outlook.com>
2019-07-08 21:20:20 +0200

    Importante... ya funciona el método guardarPosicionesArray

--- JuegoDelAhorcado/js/app.js
+++ JuegoDelAhorcado/js/app.js
@@ -18,18 +18,41 @@ const arrayTextoSolucion = textMin2.split(" ");
 
 // Creamos los botones
 const abecedario = "A B C D E F G H I J K L M N Ñ O P Q R S T U V W X Y Z";
-const arrayABC = abecedario.split(" ");
+const arrayABC = abecedario.toLowerCase().split(" ");
+
+console.log(arrayABC);
 let j = 0;
 
+// Función que genera los botones del abecedario
 function generaABC() {
     for (i = 0; i < 27; i++) {
 
         const btn = document.createElement("button");
         btn.type = "button";
         btn.textContent = arrayABC[i];
-        btn.id = arrayABC[i].toLowerCase();
+        btn.id = arrayABC[i];
         divBtn.appendChild(btn);
     }
 }
 
-window.onload = generaABC();
+// Función que guarda las posiciones que hay en el array del caracter que busca
+function guardaPosicionesArray(array, caracter) {
+
+    const arrayPosiciones = [];
+    let idx = array.indexOf(caracter);  // Primer caracter encontrado
+
+    while(idx != -1){  // Mientras que no de un -1 en la búsqueda del caracter
+        
+        arrayPosiciones.push(idx); // Vamos metiendo las posiciones en el arrayPosiciones
+        idx = array.indexOf(caracter, idx + 1); // Y seguimos buscando en el array apartir de la posición anterior
+    }
+    
+    return arrayPosiciones;
+}
+
+// Inicio de la app
+function inicio() {
+    generaABC();
+}
+
+window.onload = inicio();
