481f9bdaaba59439ff754a85e1603dfdb2f1b804
rpf1890 <united_ssh@outlook.com>
2019-07-08 12:51:52 +0200

    Creamos botones con DOM

--- JuegoAhorcado/js/app.js
+++ JuegoAhorcado/js/app.js
@@ -1,9 +1,9 @@
-const idLetra = document.getElementById("idLetra");
-const parrafo = document.getElementById("idTexto");
+const abcBtn = document.getElementById("idDivAbecedario");
+const textoJuego = document.getElementById("idTexto");
 const idDivPrincipal = document.getElementById("idDivPrincipal");
 
 // Guardamos texto del párrafo tal como se muestra en html 
-const textoPalabra = parrafo.textContent;
+const textoPalabra = textoJuego.textContent;
 
 // Guardamos texto completo de la palabra ( solución )
 const textoSolucion = "E l e c t r o d o m e s t i c o";
@@ -16,8 +16,11 @@ const textMin2 = textoSolucion.toLowerCase();
 const arrayTextoJuego = textMin.split(" ");
 const arrayTextoSolucion = textMin2.split(" ");
 
+console.log(arrayTextoJuego);
+console.log(arrayTextoSolucion);
 
-idLetra.addEventListener('keypress', function (event) {
+
+/* idLetra.addEventListener('keypress', function (event) {
     // Valor de la letra que escribimos en el input
     const inputChar = event.target.value;
 
@@ -34,31 +37,40 @@ idLetra.addEventListener('keypress', function (event) {
         }
 
     }
-})
+}) */
 
 // Funciones aparte
-function arrayPosicionesCaracter(arrayOriginal, char)
-{   // Con esta función pretendemos guardar todas las posiciones que
-    // ocupan en el array el caracter que pasamos por parámetro
 
-    let pos = -1;
-    let arrayDePosiciones = new Array();
+// Función que nos crea los botones del abecedario
+function creaAbecedarioBtn(a, z) {
 
-    for(i = 0; i < arrayOriginal.length; i++)
-    {
-        if(arrayOriginal[i] === char) // Si una de las posiciones del array es el caracter (será la primera que encuentre)
-        {
-            pos = arrayOriginal.indexOf(arrayOriginal[i]);  // Tomamos la posición del caracter ( caracter del array que coincide con el caracter que vamos buscando )
-            arrayDePosiciones.push(pos); // Guardamos la posición en el array
+    let i = a.charCodeAt(0);
+    let j = z.charCodeAt(0);
+    let letra = "";
+    let idBtn = 1;
 
-            console.log(arrayDePosiciones);
-            arrayOriginal.splice(pos,1,"x"); // En este punto debemos sustituir el caracter por otro ( ejemplo una x ) ya que indexOf solo devuelve la posición del primer caracter encontrado y queremos todas las posiciones por si hay más caracteres iguales
+    for (; i <= j; i++) {
 
-        }
+        letra = String.fromCharCode(i).toUpperCase();
 
-        arrayPosicionesCaracter(arrayOriginal, char); // Llamada recursiva de la función para que siga hasta que acaben las interaciones del for
+        const btn = document.createElement("button");
+        btn.type = "button";
+        btn.textContent = letra;
+        btn.id = idBtn;
+        idBtn++;
+        document.body.appendChild(btn);
     }
-
-    return arrayDePosiciones;
-    
 }
+
+// Creamos botones del abecedario
+function inicio() {
+    creaAbecedarioBtn("a", "z");
+}
+
+// Inicio 
+window.onload = inicio();
+
+
+
+
+
