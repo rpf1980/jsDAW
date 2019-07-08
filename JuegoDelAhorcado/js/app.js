f49e0ce4c1439881c0760e5f5ec73e55b569e20c
rpf1980 <united_ssh@outlook.com>
2019-07-08 21:52:22 +0200

    Revisar función posicionaCaracteres

--- JuegoDelAhorcado/js/app.js
+++ JuegoDelAhorcado/js/app.js
@@ -41,13 +41,44 @@ function guardaPosicionesArray(array, caracter) {
     const arrayPosiciones = [];
     let idx = array.indexOf(caracter);  // Primer caracter encontrado
 
-    while(idx != -1){  // Mientras que no de un -1 en la búsqueda del caracter
-        
+    while (idx != -1) {  // Mientras que no de un -1 en la búsqueda del caracter
+
         arrayPosiciones.push(idx); // Vamos metiendo las posiciones en el arrayPosiciones
         idx = array.indexOf(caracter, idx + 1); // Y seguimos buscando en el array apartir de la posición anterior
     }
-    
+
     return arrayPosiciones;
+}
+
+// Evento de los botones
+const buttons = document.querySelectorAll("button"); // Seleccionamos todos los botones
+
+buttons.forEach(function (button) {
+
+    button.addEventListener('click', function (event) {
+
+        const charBtn = event.target.id; // Recordemos que los id's de los botones corresponden al propio caracter que se busca
+
+
+    })
+})
+
+// Pinta caracteres en determinadas posiciones
+function posicionaCaracteres(array, arrayPos, caracter) {
+
+    // Nuestra ferencia será el array de posiciones
+    for (i = 0; i < array.length; i++) {
+        for (j = 0; j < arrayPos.length; j++) {
+            if (i == j) {
+
+                array.splice(i, 1, caracter); // Sobre la posición en array sustituye lo que tenga por el caracter que pasamos por parámetro
+            }
+
+        }
+
+    }
+
+    return array; // Devolvemos el array con los cambios
 }
 
 // Inicio de la app
